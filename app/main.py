948ed8765aba193e3d9e5031fee54f59d9560089
Kateryna Cherepanova <kateryna.cherepanova.12@gmail.com>
2026-08-22 20:32:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    command_parts = command.split()
+
+    if len(command) != 3:
+        return
+
+    operation, source, destination = command_parts
+
+    if (
+        operation != "cp"
+        or source == destination
+        or not os.path.isfile(source)
+    ):
+        return
+
+    with open(source, "r") as file_in, open(destination, "w") as file_out:
+        file_out.write(file_in.read())
